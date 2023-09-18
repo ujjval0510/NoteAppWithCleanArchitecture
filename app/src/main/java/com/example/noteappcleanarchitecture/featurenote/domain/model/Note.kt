79a8c9b868e44407003b0abe3c93766125bb25a7
ujjval shrimali <ujjvalshri0510@gmail.com>
2023-09-18 20:24:33 +0900

    Change Icon and name of the application
    Change Icon and name of the application

--- app/src/main/java/com/example/noteappcleanarchitecture/featurenote/domain/model/Note.kt
+++ app/src/main/java/com/example/noteappcleanarchitecture/featurenote/domain/model/Note.kt
@@ -13,6 +13,7 @@ data class Note(
     val title: String,
     val content: String,
     val timeStamp: Long,
+    val createdDate: String,
     val color: Int,
     @PrimaryKey val id: Int? = null
 ) {
