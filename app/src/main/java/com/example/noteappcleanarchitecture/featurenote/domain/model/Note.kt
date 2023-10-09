4fdb56f8371086a7f7aa877bbc482c8908f1cfb7
ujjval shrimali <ujjvalshri0510@gmail.com>
2023-10-09 21:45:01 +0900

    update note.kt

--- app/src/main/java/com/example/noteappcleanarchitecture/featurenote/domain/model/Note.kt
+++ app/src/main/java/com/example/noteappcleanarchitecture/featurenote/domain/model/Note.kt
@@ -13,7 +13,6 @@ data class Note(
     val title: String,
     val content: String,
     val timeStamp: Long,
-    val createdDate: String,
     val color: Int,
     @PrimaryKey val id: Int? = null
 ) {
